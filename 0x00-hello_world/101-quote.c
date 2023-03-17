7a164a703fd7338e4d5687e53daabee49b82c251
Darry Mbae <darrylmbae01@gmail.com>
2023-03-17 23:51:38 +0000

    updating 101-quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+
+/**
+ * main - Prints a string to standard error
+ * Return: 1
+ */
+int main(void)
+{
+  char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+  int len = sizeof(str) - 1;
+  int ret_val;
+
+  ret_val = write(STDERR_FILENO, str, len);
+  if (ret_val != len)
+    return (1);
+
+  return (1);
+}
