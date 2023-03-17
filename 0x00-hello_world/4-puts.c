1d0c20f8043b2c4e21dbb19d41a9661be6be49e4
Darry Mbae <darrylmbae01@gmail.com>
2023-03-17 22:52:40 +0000

    updating 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,10 @@
 int main(void)
 {
 	/**
+	 * main - entry point
 	 * Description: prints Programming is like building a multilingual puzzle
 	 * returns a value of 0
 	 */
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
