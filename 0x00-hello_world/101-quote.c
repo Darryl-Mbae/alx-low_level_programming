10a561602381d8fc013453efa5723f1f8479586d
Darry Mbae <darrylmbae01@gmail.com>
2023-03-18 06:40:37 +0000

    updating 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,8 @@
 int main(void)
 {
 	const char* message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	syscall(SYS_write, 1, message, sizeof(message));;
+
+	syscall(SYS_write, 2, message, sizeof(message) - 1);
 	return (1);
 }
 
