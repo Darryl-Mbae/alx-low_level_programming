5ca6d82dd90dac5b82d4dd562aa96b023220d70b
Darry Mbae <darrylmbae01@gmail.com>
2023-03-18 06:45:40 +0000

    updating 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,16 @@
 #include <unistd.h>
 #include <sys/syscall.h>
+#include <string.h>
 /**
  * main - Prints a string to standard error
  * Return: 1
  */
 int main(void)
 {
-	const char* message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	size_t message_len = strlen(message);
 
-	syscall(SYS_write, 2, message, sizeof(message) - 1);
+	syscall(SYS_write, 2, message, message_len);
 	return (1);
 }
 
