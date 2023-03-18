ab7b4bebefc1fb81acb203ca0787c1384c7220da
Darry Mbae <darrylmbae01@gmail.com>
2023-03-18 10:48:35 +0000

    Adding 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,17 +9,17 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i < 100; i++)
 	{
-		for (j = 0; j < 10; j++)
+		for (j = 0; j < 100; j++)
 		{
-			putchar(i + '0');
-			putchar(j + '0');
-			if (i != 9 || j != 9)
-			{
-				putchar(',');
-				putchar(' ');
-			}
+			putchar(i/10 + '0');
+			putchar(i%10 + '0');
+			putchar(' ');
+			putchar(j/10 + '0');
+			putchar(j%10 + '0');
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
