eeb094991c140ac52e25383ab5d75c4bb04ce4d3
Darry Mbae <darrylmbae01@gmail.com>
2023-03-18 06:27:36 +0000

    updating 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,15 +4,9 @@
  * main - Prints a string to standard error
  * Return: 1
  */
-int main(void)
-{
-	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	int len = sizeof(str) - 1;
-	int ret_val;
-
-	ret_val = write(STDERR_FILENO, str, len);
-	if (ret_val != len)
-		return (1);
-
-	return (1);
+int main() {
+	const char* message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, message, 47);
+	return 1;
 }
+
