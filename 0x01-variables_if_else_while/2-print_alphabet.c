c7b8a5daa2aa881f07f45f5de0ea714bb298302b
Darry Mbae <darrylmbae01@gmail.com>
2023-03-18 09:03:07 +0000

    updating 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,7 +8,7 @@
  * Return: Always 0 (Success)
  */
 
-int main() 
+int main(void) 
 {
 	char letter;
 	
@@ -17,5 +17,5 @@ int main()
 		putchar(letter);
 	}
 	putchar('\n');
-	return 0;
+	return (0);
 }
