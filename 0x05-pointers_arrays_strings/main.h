7ff5eb4870c341713650b3ab68f049cbd59e875d
Darryl Mbae <darrylmbae01@gmail.com>
2023-03-27 17:28:11 +0000

    Adding remaining task at a go

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -7,4 +7,9 @@ void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
+void rev_string(char *s);
+char *_strcpy(char *dest, char *src);
+void print_array(int *a, int n);
+void puts_half(char *str);
+void puts2(char *str);
 #endif
