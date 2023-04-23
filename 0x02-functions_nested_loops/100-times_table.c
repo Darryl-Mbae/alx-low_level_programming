8719d160f49a1a86dfba014efb5733c97079ae59
Darryl Mbae <darrylmbae01@gmail.com>
2023-04-23 08:37:32 -0700

    Fixing errors

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,32 +1,50 @@
 #include "main.h"
 
 /**
- * print_times_table - prints multiplication tabel starting from 0 to n number
- * @n: The highest number in the multiplication table.
+ * print_times_table - prints the n times table, starting with 0
+ * @n: number of the times table
  * Description: This function prints a multiplication table starting from 0
  * and going up to the specified number (n). The function ensures that the
  * table is not printed if n is greater than or equal to 15 or less than 1.
  */
 void print_times_table(int n)
 {
-	if (n < 15 && n > 0)
-	{
-		int i, j;
+	int i, j, k;
 
+	if (n >= 0 && n <= 15)
+	{
 		for (i = 0; i <= n; i++)
 		{
 			for (j = 0; j <= n; j++)
 			{
-				int r = j * i;
-
-				printf("%2d", r);
-				if (j >= 0)
+				k = j * i;
+				if (j == 0)
 				{
-					putchar(',');
-					putchar(' ');
+					_putchar(k + '0');
+				} else if (k < 10 && j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(k + '0');
+				} else if (k >= 10 && k < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
+				} else if (k >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((k / 100) + '0');
+					_putchar(((k / 10) % 10) + '0');
+					_putchar((k % 10) + '0');
 				}
 			}
-			printf("\n");
+			_putchar('\n');
 		}
 	}
 }
